cfbb621d416bca36a3d003b6638d59268f08fb93
Rajabrahmam Thanniru <130306764+Rajabrahmam-Thanniru@users.noreply.github.com>
2026-06-13 15:21:14 +0530

    fix: update authorization roles for category and course category routes

--- src/Users/Admin/Category/Category.Route.ts
+++ src/Users/Admin/Category/Category.Route.ts
@@ -9,13 +9,13 @@ const CourseCategoryRouter = Router();
 const categoryObj = new CompanyCategoryController();
 const CourseCategoryObj = new CourseCategoryController()
 
-categoryRouter.get("/map", AuthController.middleWare, AuthController.authorize(Role.COMPANY, Role.ADMIN), (req, res) => categoryObj.GetCompanyCatogryasMap(req, res));
+categoryRouter.get("/map", AuthController.middleWare, AuthController.authorize(Role.COMPANY, Role.ADMIN, Role.ADMIN_INTERN), (req, res) => categoryObj.GetCompanyCatogryasMap(req, res));
 categoryRouter.get("/", AuthController.middleWare, AuthController.authorize(Role.ADMIN), (req, res) => categoryObj.getAllCategories(req, res));
 categoryRouter.get("/:id", AuthController.middleWare, AuthController.authorize(Role.ADMIN), (req, res) => categoryObj.getCategoryById(req, res));
 categoryRouter.post("/", AuthController.middleWare, AuthController.authorize(Role.ADMIN), (req, res) => categoryObj.createCategory(req, res));
 
 CourseCategoryRouter.get("/map", (req, res) => CourseCategoryObj.getAllCourseCategoryMap(req, res));
-CourseCategoryRouter.get("/map/:companyId", AuthController.middleWare, AuthController.authorize(Role.COMPANY, Role.ADMIN), (req, res) => CourseCategoryObj.getCourseCategoryMap?.(req, res));
+CourseCategoryRouter.get("/map/:companyId", AuthController.middleWare, AuthController.authorize(Role.COMPANY, Role.ADMIN, Role.ADMIN_INTERN), (req, res) => CourseCategoryObj.getCourseCategoryMap?.(req, res));
 CourseCategoryRouter.get("/:companyId", AuthController.middleWare, AuthController.authorize(Role.ADMIN), (req, res) => CourseCategoryObj.getAllCourseCategories(req, res));
 CourseCategoryRouter.get("/:categoryId", AuthController.middleWare, AuthController.authorize(Role.ADMIN), (req, res) => CourseCategoryObj.getCourseCategoryById(req, res));
 CourseCategoryRouter.post("/", AuthController.middleWare, AuthController.authorize(Role.ADMIN), (req, res) => CourseCategoryObj.createOrUpdateCourseCategory(req, res));
