3a1dbf1cdeae6461edf4d9daf39704c95359d7d5
Rajabrahmam Thanniru <130306764+Rajabrahmam-Thanniru@users.noreply.github.com>
2026-06-13 13:57:28 +0530

    fix: specify storage bucket during Firebase initialization

--- src/Connect/firebaseAdmin.ts
+++ src/Connect/firebaseAdmin.ts
@@ -2,7 +2,9 @@ import admin from "firebase-admin";
 
 
 if (!admin.apps.length) {
-  admin.initializeApp();
+  admin.initializeApp({
+    storageBucket: "learner-space-e08d4.firebasestorage.app",
+  });
 }
 
 const bucket = admin.storage().bucket();
